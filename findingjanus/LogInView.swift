628aaf51f90af96a3fe1ea594c0722160fe3ac3b
Brian <brpereir@ucsc.edu>
2020-05-14 21:43:47 -0700

    update

--- findingjanus/LogInView.swift
+++ findingjanus/LogInView.swift
@@ -11,44 +11,148 @@ import SwiftUI
 struct LogInView: View {
     var body: some View {
         ZStack{
-                   Color.offBlack
+            Color.offBlack
             VStack(spacing: 30) {
                 RoundedRectangle(cornerRadius: 25)
-                .fill(Color.offBlack)
-                .frame(width: 275)
-                
-                .frame(height: 275)
-                .shadow(color: Color.black.opacity(0.7),
-                        radius: 10, x: 10, y: 10)
-                .shadow(color: Color.white.opacity(0.1),
-                        radius: 6, x: -4, y: -4)
+                    .fill(Color.offBlack)
+                    .frame(width: 275)
+                    
+                    .frame(height: 275)
+                    .shadow(color: Color.black.opacity(0.7),
+                            radius: 10, x: 10, y: 10)
+                    .shadow(color: Color.white.opacity(0.1),
+                            radius: 6, x: -4, y: -4)
                     .offset(x: 0, y: -120)
-                VStack(spacing: 10){
-                LogIn()
-                ZStack {
-                                                 
-                                                 
-                                                
-                                                 RoundedRectangle(cornerRadius: 100)
-                                                     .fill(Color.offBlack)
-                                                   
-                                                 .overlay(
-                                                     RoundedRectangle(cornerRadius: 100)
-                                                         .stroke(Color.black,lineWidth: 5)
-                                                         .blur(radius: 4)
-                                                         .offset(x: 2, y: 2)
-                                                         .mask(RoundedRectangle(cornerRadius: 100)
-                                                             .fill(LinearGradient(Color.offBlack, Color.clear)))
-                                                 
-                                                 )
+                VStack(spacing: 20){
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 100)
+                            .fill(Color.offBlack)
+                            
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.black,lineWidth: 4)
+                                    .blur(radius: 6)
+                                    .offset(x: 2, y: 2)
+                                    .mask(RoundedRectangle(cornerRadius: 100)
+                                        .fill(LinearGradient(Color.offBlack, Color.clear)))
+                                
+                        )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.black,lineWidth: 4)
+                                    .blur(radius: 6)
+                                    .offset(x: -2, y: -2)
+                                    .mask(RoundedRectangle(cornerRadius: 100)
+                                        .fill(LinearGradient(Color.clear, Color.offBlack)))
+                                
+                        )
                     }
-                                               .frame(width: 350, height: 8)
-               
-                LogIn()
+                    .frame(width: 380, height: 3)
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 100)
+                            .fill(Color.offBlack)
+                            
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.black,lineWidth: 4)
+                                    .blur(radius: 6)
+                                    .offset(x: 2, y: 2)
+                                    .mask(RoundedRectangle(cornerRadius: 100)
+                                        .fill(LinearGradient(Color.offBlack, Color.clear)))
+                                
+                        )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.black,lineWidth: 4)
+                                    .blur(radius: 6)
+                                    .offset(x: -2, y: -2)
+                                    .mask(RoundedRectangle(cornerRadius: 100)
+                                        .fill(LinearGradient(Color.clear, Color.offBlack)))
+                                
+                        )
+                    }
+                    .frame(width: 360, height: 3)
+                    
+                    LogIn()
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 100)
+                            .fill(Color.offBlack)
+                            
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.black,lineWidth: 4)
+                                    .blur(radius: 4)
+                                    .offset(x: 2, y: 2)
+                                    .mask(RoundedRectangle(cornerRadius: 100)
+                                        .fill(LinearGradient(Color.offBlack, Color.clear)))
+                                
+                        )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.black,lineWidth: 4)
+                                    .blur(radius: 4)
+                                    .offset(x: -2, y: -2)
+                                    .mask(RoundedRectangle(cornerRadius: 100)
+                                        .fill(LinearGradient(Color.clear, Color.offBlack)))
+                                
+                        )
+                    }
+                    .frame(width: 300, height: 3)
+                    
+                    LogIn()
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 100)
+                            .fill(Color.offBlack)
+                            
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.black,lineWidth: 4)
+                                    .blur(radius: 6)
+                                    .offset(x: 2, y: 2)
+                                    .mask(RoundedRectangle(cornerRadius: 100)
+                                        .fill(LinearGradient(Color.offBlack, Color.clear)))
+                                
+                        )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.black,lineWidth: 4)
+                                    .blur(radius: 6)
+                                    .offset(x: -2, y: -2)
+                                    .mask(RoundedRectangle(cornerRadius: 100)
+                                        .fill(LinearGradient(Color.clear, Color.offBlack)))
+                                
+                        )
+                    }
+                    .frame(width: 360, height: 3)
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 100)
+                            .fill(Color.offBlack)
+                            
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.black,lineWidth: 4)
+                                    .blur(radius: 6)
+                                    .offset(x: 2, y: 2)
+                                    .mask(RoundedRectangle(cornerRadius: 100)
+                                        .fill(LinearGradient(Color.offBlack, Color.clear)))
+                                
+                        )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 100)
+                                    .stroke(Color.black,lineWidth: 4)
+                                    .blur(radius: 6)
+                                    .offset(x: -2, y: -2)
+                                    .mask(RoundedRectangle(cornerRadius: 100)
+                                        .fill(LinearGradient(Color.clear, Color.offBlack)))
+                                
+                        )
+                    }
+                    .frame(width: 380, height: 3)
+                }
+                
             }
-            
-            }
-        .offset(x: 0, y: 100)
+            .offset(x: 0, y: 100)
         }
         .edgesIgnoringSafeArea(.all)
     }
@@ -63,54 +167,54 @@ struct LogInView_Previews: PreviewProvider {
 struct LogIn: View {
     var body: some View {
         ZStack {
-                          VStack {
-                              RoundedRectangle(cornerRadius: 100)
-                                                     .fill(Color.offBlack)
-                                                  
-                                                     .shadow(color: Color.black.opacity(0.8),
-                                                             radius: 10, x: 10, y: 10)
-                                                     .shadow(color: Color.white.opacity(0.1),
-                                                         radius: 6, x: -4, y: -4)
-                          }
-                          .frame(width: 310, height: 45)
-                         
-                          VStack {
-                              ZStack {
-                                  
-                                  
-                                 
-                                  RoundedRectangle(cornerRadius: 100)
-                                      .fill(Color.offBlack)
-                                    
-                                  .overlay(
-                                      RoundedRectangle(cornerRadius: 100)
-                                          .stroke(Color.black,lineWidth: 5)
-                                          .blur(radius: 4)
-                                          .offset(x: 2, y: 2)
-                                          .mask(RoundedRectangle(cornerRadius: 100)
-                                              .fill(LinearGradient(Color.offBlack, Color.clear)))
-                                  
-                                  )
-                                  .overlay(
-                                      RoundedRectangle(cornerRadius: 100)
-                                      .stroke(Color.black, lineWidth: 6)
-                                      .blur(radius: 4)
-                                      .offset(x: -2, y: -2)
-                                          .mask(RoundedRectangle(cornerRadius: 100)
-                                              .fill(LinearGradient(Color.clear, Color.black.opacity(0.9))))
-                                  )
-                                    
-                              }
-                              .frame(width: 300, height: 35)
-                             
-                              
-                              
-                             
-                          }
-
-                      }
+            VStack {
+                RoundedRectangle(cornerRadius: 100)
+                    .fill(Color.offBlack)
+                    
+                    .shadow(color: Color.black.opacity(0.8),
+                            radius: 10, x: 10, y: 10)
+                    .shadow(color: Color.white.opacity(0.1),
+                            radius: 6, x: -4, y: -4)
+            }
+            .frame(width: 310, height: 45)
+            
+            VStack {
+                ZStack {
+                    
+                    
+                    
+                    RoundedRectangle(cornerRadius: 100)
+                        .fill(Color.offBlack)
+                        
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 100)
+                                .stroke(Color.black,lineWidth: 5)
+                                .blur(radius: 4)
+                                .offset(x: 2, y: 2)
+                                .mask(RoundedRectangle(cornerRadius: 100)
+                                    .fill(LinearGradient(Color.offBlack, Color.clear)))
+                            
+                    )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 100)
+                                .stroke(Color.black, lineWidth: 6)
+                                .blur(radius: 4)
+                                .offset(x: -2, y: -2)
+                                .mask(RoundedRectangle(cornerRadius: 100)
+                                    .fill(LinearGradient(Color.clear, Color.black.opacity(0.9))))
+                    )
+                    
+                }
+                .frame(width: 300, height: 35)
+                
+                
+                
+                
+            }
+            
+        }
         
         
- 
+        
     }
 }
