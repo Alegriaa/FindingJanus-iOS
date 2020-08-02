f1e9b694ea769e59753a7254cbe278edafce056d
Brian <brpereir@ucsc.edu>
2020-08-02 11:43:13 -0700

    scroll time idea

---
+++ findingjanus/NotesView.swift
@@ -0,0 +1,26 @@
+//
+//  NotesView.swift
+//  findingjanus
+//
+//  Created by NoStress on 8/2/20.
+//  Copyright © 2020 Finding Janus. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+//        I can possibly add a right scroll menu type of list
+//        to see the previous day, the present day, and tmrw.
+//        i think can be cool and can be modified well.
+//        
+    }
+}
+
+struct NotesView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotesView()
+    }
+}
